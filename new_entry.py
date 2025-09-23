6ec4933fbe5f43e915b4718da0f986a7be180a0f
ptrsym <ptr.sym@gmail.com>
2025-09-24 00:54:25 +1000

    Added entry script and first entry

---
+++ new_entry.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import re
+import os
+
+def safe_filename(title: str) -> str:
+    title = title.strip().lower()
+    title = title.replace(" ", "_").replace("/", "-")
+    title = re.sub(r'[^a-z0-9_\-]', '', title)
+    return title
+
+now = datetime.now()
+date_str = now.strftime("%Y-%m-%d %H-%M-%S")
+year_str = now.strftime("%Y")
+time_str = now.strftime("%H:%M")
+
+entry_title = input("Entry title: ")
+safe_title = safe_filename(entry_title)
+
+if not os.path.exists(year_str):
+    os.makedirs(year_str)
+
+filename = f"{date_str}_{safe_title}.md" if safe_filename else f"{date_str}.md"
+file_path = os.path.join(year_str, filename)
+
+with open(file_path, 'w') as file:
+    file.write(f"# {entry_title}\n\n")
+    file.write(f"Date:* {now.strftime('%Y-%m-%d')}\n")
+    file.write(f"Time:* {time_str}\n\n")
+    file.write("---\n\n")
+    file.write("Thoughts...\n")
+
+print(f"Created journal entry: {file_path}")
---
+++ readme.md
@@ -0,0 +1,6 @@
+# My Journal
+
+## Purpose
+
+I'm starting a journal predominately to help structure my learning/life goals and hold myself accountable daily to meeting them.
+I'll be focusing on my programming journey but may include health/mindset reflections if appropriate.
